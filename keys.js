10d0d9935cb79dea5c8d670434014e8d1d17d8a3
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-09-10 21:24:33 +0100

    Keys enum

---
+++ keys.js
@@ -0,0 +1,11 @@
+const Keys = {
+	SPACE: " ",
+	LEFT_ARROW: "ArrowLeft",
+	UP_ARROW: "ArrowUp",
+	RIGHT_ARROW: "ArrowRight",
+	DOWN_ARROW: "ArrowDown",
+	KEY_W: "w",
+	KEY_A: "a",
+	KEY_S: "s",
+	KEY_D: "d",
+};
