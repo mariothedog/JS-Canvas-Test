4914df726dff12c3c41f4efb4674ba6d0243474b
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-09-10 22:08:09 +0100

    Made GameObject abstract

--- game-object.js
+++ game-object.js
@@ -3,5 +3,9 @@ class GameObject {
 		this.canvasContext = canvasContext;
 		this.position = position;
 		this.velocity = velocity;
+
+		if (this.constructor === GameObject) {
+			throw new Error("Cannot instantiate abstract class");
+		}
 	}
 }
