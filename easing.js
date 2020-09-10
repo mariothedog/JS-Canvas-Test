697b9d850852211736aa0e8cf217941aa0587423
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-09-10 17:46:49 +0100

    Easing functions

---
+++ easing.js
@@ -0,0 +1,10 @@
+function easeLinear(t, b, c, d) {
+	return c * t / d + b;
+}
+
+function easeInOutQuint(t, b, c, d) {
+	if ((t /= d / 2) < 1) {
+		return c / 2 * t * t * t * t * t + b;
+	}
+	return c / 2 * ((t -= 2) * t * t * t * t + 2) + b;
+}
